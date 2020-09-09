9ea8a69291421808cce4d43b87860b1eff2f6f38
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-09 14:56:53 +0100

    Update app.module.ts
    + Restructure of imports to make visual sense to me
    + Restructure of declarations and imports for the same reason as above

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { QuestionFormComponent } from './question-form/question-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ResultsComponent } from './results/results.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ResultsComponent } from './results/results.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent, ResultsComponent, WelcomeComponent, QuestionsComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    QuestionsComponent,
+    QuestionFormComponent,
+    WelcomeComponent,
+    ResultsComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
