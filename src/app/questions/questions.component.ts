ab56d7c80f3a6efe156f31a5531e99512c7add9b
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-09 14:42:47 +0100

    Updated to imports, exports and constructor

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+
+import { QuestionsService } from '../questions.service';
+import { Quiz, Answers, Choice, Question } from '../quiz.model';
 
 @Component({
-  selector: 'app-questions',
-  templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
+// export class AppComponent {
 export class QuestionsComponent implements OnInit {
+  quiz: Quiz;
+  answers: Answers;
+  questions: Question[];
+  currentQuestionIndex: number;
 
-  constructor() { }
+  private showResults = false;
 
-  ngOnInit(): void {
+  // constructor(private questionsService: QuestionsService) {
+  //   this.questionsService.getJSON('game').subscribe((data) => {
+  //     this.quiz = new Quiz('game', data);
+  //     this.answers = new Answers();
+  //     this.currentQuestionIndex = 0;
+  //   });
+  // }
+  constructor(
+    private route: ActivatedRoute,
+    private questionsService: QuestionsService
+  ) {}
+
+  ngOnInit() {
+    this.questionsService
+      .getQuestions(this.route.snapshot.params.quizId)
+      .subscribe((questions) => {
+        this.questions = questions;
+        this.answers = new Answers();
+        this.currentQuestionIndex = 0;
+      });
   }
 
+  updateChoice(choice: Choice) {
+    this.answers.values[this.currentQuestionIndex] = choice;
+  }
+
+  nextOrViewResults() {
+    if (this.currentQuestionIndex === this.questions.length - 1) {
+      this.showResults = true;
+      return;
+    }
+
+    this.currentQuestionIndex++;
+  }
+  reset() {
+    this.quiz = undefined;
+    this.questions = undefined;
+    this.answers = undefined;
+    this.currentQuestionIndex = undefined;
+  }
 }
