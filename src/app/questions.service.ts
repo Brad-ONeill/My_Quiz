edeb203e4aa810e0cec6fa799e4a56aa665f1b04
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-09 14:19:31 +0100

    Changes to return a list of files instead of a single file

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Quiz, Question } from './quiz.model';
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,32 @@ import { Observable } from 'rxjs';
 export class QuestionsService {
   constructor(private http: HttpClient) {}
 
-  public getJSON(fileId: string) {
-    return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
+  // public getJSON(fileId: string) {
+  //   return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
+  // }
+  public getQuizzes() {
+    return this.http.get(`./assets/quiz-list.json`).pipe(
+      map((result: any[]) => {
+        return result.map(
+          (result) =>
+            new Quiz(
+              result.label,
+              result.name,
+              result.description,
+              result.fileName
+            )
+        );
+      })
+    );
+  }
+
+  public getQuestions(fileName: string) {
+    return this.http.get(`./assets/${fileName}.json`).pipe(
+      map((result: any[]) => {
+        return result.map(
+          (esult) => new Question(result.label, result.choices)
+        );
+      })
+    );
   }
 }
