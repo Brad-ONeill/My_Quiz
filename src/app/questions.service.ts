692466fa28ec48de5c6b44154459c1119e163679
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-09 14:58:11 +0100

    Update questions.service.ts

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Quiz, Question } from './quiz.model';
-// import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +13,7 @@ export class QuestionsService {
   // public getJSON(fileId: string) {
   //   return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
   // }
+
   public getQuizzes() {
     return this.http.get(`./assets/quiz-list.json`).pipe(
       map((result: any[]) => {
@@ -33,7 +34,7 @@ export class QuestionsService {
     return this.http.get(`./assets/${fileName}.json`).pipe(
       map((result: any[]) => {
         return result.map(
-          (esult) => new Question(result.label, result.choices)
+          (result) => new Question(result.label, result.choices)
         );
       })
     );
