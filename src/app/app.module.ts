1d91519f6c8846fccd908cc9f4b77f8bac6f99bd
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-09 14:34:00 +0100

    Creation and initial commit of questions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { QuestionFormComponent } from './question-form/question-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ResultsComponent } from './results/results.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent, ResultsComponent, WelcomeComponent],
+  declarations: [AppComponent, QuestionFormComponent, ResultsComponent, WelcomeComponent, QuestionsComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
